5dfb67a33eb61b97febcbf317a630910de90b84c
ngquangphuoc <phuocnguyen061@gmail.com>
2017-10-16 12:03:04 +0700

    test_upload_controller

---
+++ application/controllers/admin/Upload.php
@@ -0,0 +1,31 @@
+<?php
+Class Upload extends MY_Controller
+{
+    function index()
+    {
+        if($this->input->post('submit'))
+        {
+            $this->load->library('upload_library');
+            $upload_path = './upload/schedule';
+            $data = $this->upload_library->upload($upload_path, 'image');
+            pre($data);
+        }
+        $this->data['temp'] = 'admin/upload/index';
+        $this->load->view('admin/main', $this->data);
+    }
+    
+    // function upload_file()
+    // {
+    //     if($this->input->post('submit'))
+    //     {
+    //        $this->load->library('upload_library');
+    //        $upload_path = './upload/user';
+    //        $data = $this->upload_library->upload_file($upload_path, 'image_list');
+    //        pre($data);
+    //     }
+        
+    //     $this->data['temp'] = 'admin/upload/upload_file';
+    //     $this->load->view('admin/main', $this->data);
+    // }
+    
+}
