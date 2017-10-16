6af7fe268e9f463ac426b95766d3c4f5093980b3
ngquangphuoc <phuocnguyen061@gmail.com>
2017-10-16 12:05:29 +0700

    tao ra cac ham dung chung cho admin

--- application/helpers/commom_helper.php
+++ application/helpers/commom_helper.php
@@ -3,4 +3,13 @@ function public_url($url = '')
 {
 	return base_url('public/'.$url);
 }
+function pre($list, $exit = true)
+{
+    echo "<pre>";
+    print_r($list);
+    if($exit)
+    {
+        die();
+    }
+}
 ?>
