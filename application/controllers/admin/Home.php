e03f78049b2165aa91cd35bef7749b45896326de
ngquangphuoc <phuocnguyen061@gmail.com>
2017-10-27 16:46:27 +0700

    Tich_hop_code_chung

--- application/controllers/admin/Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-Class Home extends MY_Controller
-{
-    function index()
-    {
-        $this->data['temp'] = 'admin/home/index';
-        $this->load->view('admin/main', $this->data);
-    }
-}
--- application/views/admin/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer-inner">
-    <div class="footer-content">
-        <span class="smaller-90">
-            &copy; Học Bạ Điện Tử 2017
-        </span>
-    </div>
-</div>
