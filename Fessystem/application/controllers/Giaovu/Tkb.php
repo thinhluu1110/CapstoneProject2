be91f31d4b5454d1efcde3424c3513ed3acb258f
ngquangphuoc <phuocnguyen061@gmail.com>
2017-10-30 14:26:13 +0700

    Thoikhoabieu_controller (edit fillter)

--- Fessystem/application/controllers/Giaovu/Tkb.php
+++ Fessystem/application/controllers/Giaovu/Tkb.php
@@ -4,7 +4,7 @@ Class Tkb extends MY_Controller
     function __construct()
     {
         parent::__construct();
-        //load ra file model
+        //load ra file model thoi khoa bieu
         $this->load->model('thoikhoabieu_model');
     }
 
@@ -13,23 +13,54 @@ Class Tkb extends MY_Controller
      */
     function index()
     {
-        // //load ra thu vien phan trang
-        // $this->load->library('pagination');
-        // $config = array();
-        // $config['base_url']   = admin_url('schedule/index'); //link hien thi ra danh sach san pham
-        // //khoi tao cac cau hinh phan trang
-        // $this->pagination->initialize($config);
+        //load ra thu vien phan trang
+        $this->load->library('pagination');
+        $config = array();
+        $config['per_page']   = 10;//so luong san pham hien thi tren 1 trang
+
+        //khoi tao cac cau hinh phan trang
+        $this->pagination->initialize($config);
+        
+        $segment = $this->uri->segment(4);
+        $segment = intval($segment);
+        
+        $input = array();
+        $input['limit'] = array($config['per_page'], $segment);
+
+        //kiem tra co thuc hien loc du lieu hay khong
+        //fillter Khoa hoc
+        $sch_khoa_hoc = $this->input->get('Khoahoc');
+        $sch_khoa_hoc = intval($sch_khoa_hoc);
+        $input['where'] = array();
+        if($sch_khoa_hoc > 0)
+        {
+            $input['where']['sch_khoa_hoc'] = $sch_khoa_hoc;
+        }
+
+        //fillter Hoc ki
+        $sch_hoc_ky = $this->input->get('Hocki');
+        $sch_hoc_ky = intval($sch_hoc_ky);
+        if($sch_hoc_ky > 0)
+        {
+            $input['where']['sch_hoc_ky'] = $sch_hoc_ky;
+        }
+
+        //lay danh sach thơi khoa bieu
+        $listTkb = $this->thoikhoabieu_model->get_list($input);
+        $this->data['listTkb'] = $listTkb;
 
         //lay danh sach khoa hoc
         $this->load->model('khoahoc_model');
         $input = array();
         $listkhoahoc = $this->khoahoc_model->get_list($input);
         $this->data['listkhoahoc'] = $listkhoahoc;
-        // pre($listkhoahoc);
+
+        //lay daanh sach hoc ki
         $this->load->model('hocki_model');
         $input = array();
         $listhocki = $this->hocki_model->get_list($input);
         $this->data['listhocki'] = $listhocki;
+
         //lay nội dung của biến message
         $message = $this->session->flashdata('message');
         $this->data['message'] = $message;
@@ -58,6 +89,7 @@ Class Tkb extends MY_Controller
             {
                 $sch_image = $upload_data['file_name'];
             }
+
             //luu du lieu can them
             $data = array(
                 'sch_khoa_hoc' => $khoahoc,
@@ -73,8 +105,5 @@ Class Tkb extends MY_Controller
                 $this->session->set_flashdata('message', 'Không thêm được');
             }
         }
-
-        //load view
-
     }
 }
