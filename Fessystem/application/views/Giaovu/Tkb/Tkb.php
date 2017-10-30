79e457ba31d8f825328f526ad559dc34e4732912
ngquangphuoc <phuocnguyen061@gmail.com>
2017-10-30 14:31:33 +0700

    Fix fillter TKB

--- Fessystem/application/views/Giaovu/Tkb/Tkb.php
+++ Fessystem/application/views/Giaovu/Tkb/Tkb.php
@@ -1,48 +1,32 @@
 <div class="main-content-inner">
     <div class="page-content">
-        <!-- <img id="ajax-loader" src="http://aspnet.demo.hueuni.edu.vn/StudentManager/images/ajax-loader.gif" alt="" /> -->
         <div class="page-header">
 
             <div class="hitec-content">
                 <h2>Thời Khóa Biểu</h2>
                 <div class="container-fluid">
-                    <form method="get" action="" id="formSearch" role="form" onsubmit="return false;">
+                    <form method="get" action="<?php echo base_url('Giaovu/tkb/index')?>" id="formSearch" role="form" onsubmit="return true;">
                         <div class="row">
                             <div class="col-xs-6">
                                 <div class="input-group hitec-w-100percent">
-                                    <select class="form-control" id="khoahoc" name="KhoaHoc">
-                                        <option selected="selected" value="">--- Chọn Khóa Học ---</option>
+                                    <select class="form-control" id="Khoahoc" name="Khoahoc" onchange='if(this.value != 0) { this.form.submit(); }'>
+                                        <option value="">--- Chọn Khóa Học ---</option>
                                         <?php foreach ($listkhoahoc as $row):?>
-                                            <option value="<?php echo $row->khoahoc_id ?>"><?php echo $row->tenkhoa ?></option>
+                                            <option value="<?php echo $row->khoahoc_id ?>" <?php echo ($this->input->get('Khoahoc') == $row->khoahoc_id) ? 'selected' : ''?> ><?php echo $row->tenkhoa ?></option>
                                         <?php endforeach;?>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-xs-6">
-                              <div class="input-group hitec-w-100percent">
-                                <select class="form-control" id="Hocki" name="Hocki">
-                                    <option selected="selected" value="">--- Chọn Học Kỳ ---</option>
-                                    <?php foreach ($listhocki as $row):?>
-                                        <option value="<?php echo $row->hocki_id ?>"><?php echo $row->tenHocki ?></option>
-                                    <?php endforeach;?>
-                                </select>
-                              </div>
-                            </div>
-                            <!-- <div class="col-xs-6">
                                 <div class="input-group hitec-w-100percent">
-                                    <select class="form-control" id="khoahoc" name="KhoaHoc">
-                                        <option selected="selected" value="">--- Chọn Học Kỳ ---</option>
-                                        <option value="">1</option>
-                                        <option value="">2</option>
-                                        <option value="">3</option>
-                                        <option value="">4</option>
-                                        <option value="">5</option>
-                                        <option value="">6</option>
-                                        <option value="">7</option>
-                                        <option value="">8</option>
+                                    <select class="form-control" id="Hocki" name="Hocki" onchange='if(this.value != 0) { this.form.submit(); }'>
+                                        <option value="">--- Chọn Học Kỳ ---</option>
+                                        <?php foreach ($listhocki as $row):?>
+                                            <option value="<?php echo $row->hocki_id ?>" <?php echo ($this->input->get('Hocki') == $row->hocki_id) ? 'selected' : ''?> ><?php echo $row->tenHocki ?></option>
+                                        <?php endforeach;?>
                                     </select>
                                 </div>
-                            </div> -->
+                            </div>
                         </div>
                     </form>
                 </div>
@@ -50,9 +34,13 @@
                     <div class="container-fluid text-right">
                         <a href="javascript:void(0)"  title="Import excel" data-toggle="modal" data-target="#dialogEdit">[Import Data]</a>
                     </div>
-                    <div class="container-fluid text-center">
-                        <img src="<?php echo base_url(); ?>public/Img/TKB.png"/>
-                    </div>
+                    <?php foreach ($listTkb as $row):?>
+                        <div class="container-fluid text-center">
+                            <img src="<?php echo base_url('upload/tkb/'.$row->sch_image)?>">
+                            <br><br><br>        
+                        </div>
+                    <?php endforeach;?>
+
                 </div>
 
             </div>
@@ -61,7 +49,6 @@
                     <div id="dialogEdit" class="modal fade" tabindex="-1" style="padding-top: 0px; padding-bottom: 0px; padding-left: 17px;" aria-hidden="false">
                         <div class="modal-dialog" style="width:400px !important;height:600px !important">
                             <div class="modal-content">
-                                <!-- <form id="frmEditEmployee" name="frmCreateEmployee" class="form-horizontal" role="form" ng-submit="UpdateEmployee()"> -->
                                 <form id="frmEditEmployee" name="frmCreateEmployee" class="form-horizontal" role="form" ng-submit="UpdateEmployee()" method="post" action="<?php echo base_url('Giaovu/Tkb/add') ?>" enctype="multipart/form-data">
                                     <div class="modal-header bg-primary" style="padding:0px;text-align:center">
                                         <button type="button" class="close" data-dismiss="modal">×</button>
@@ -73,7 +60,7 @@
                                                 <div class="form-group">
                                                     <label class="coltrol-lable">Khóa:</label>
                                                     <select class="form-control" id="khoahoc" name="khoahoc">
-                                                        <option value="">--- Chọn Khóa Học ---</option>
+                                                        <option selected="selected" value="">--- Chọn Khóa Học ---</option>
                                                             <?php foreach ($listkhoahoc as $row):?>
                                                                 <option value="<?php echo $row->khoahoc_id ?>"><?php echo $row->tenkhoa ?></option>
                                                             <?php endforeach;?>
@@ -81,14 +68,14 @@
                                                 </div>
                                                 <div class="form-group">
                                                     <label class="coltrol-lable">Học Kì:</label>
-                                                    <select class="form-control" name="hocki">
+                                                    <select class="form-control" id="hocki" name="hocki">
                                                       <option selected="selected" value="">--- Chọn Học Kỳ ---</option>
                                                       <?php foreach ($listhocki as $row):?>
                                                           <option value="<?php echo $row->hocki_id ?>"><?php echo $row->tenHocki ?></option>
                                                       <?php endforeach;?>
                                                     </select>
                                                 </div>
-            									                            <div class="form-group">
+                                                                            <div class="form-group">
                                                     <div class="form-group">
                                                         <label class="coltrol-lable">File Input:</label>
                                                         <!-- <input type="file" id="exampleInputFile"> -->
